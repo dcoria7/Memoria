056e2bdf760994a8a6975d1d898e8d6ad4154109
Luis Coria <DC@dcs-macbook-pro.local>
2015-12-05 23:47:28 -0600

    game

--- MemoriaGame.playground/Contents.swift
+++ MemoriaGame.playground/Contents.swift
@@ -2,4 +2,25 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var array = 0...100
+var treintas = 30...40
+
+for i in array{
+    
+    if i >= 30 && i <= 40 {
+        if treintas.contains(i){
+            print("\(i)\t Viva Swift!!!")
+        }
+    }else if (i %  5) == 0{
+       print("\(i)\t Bingo!!!")
+    }else if i % 2 == 0{
+        print("\(i)\t par!!")
+    }else{
+        print("\(i)\t impar!!!")
+    }
+
+    
+
+    
+    
+}
